57b21ed140f2bbfb64790c9ed41c79a176c303d2
tbrunain <dev@bouh.io>
2022-10-17 10:52:50 +0200

    feat: wip

--- redstone_api/src/lib.rs
+++ redstone_api/src/lib.rs
@@ -1,7 +1,14 @@
+//! Everything related to [`Redstone`]'s api
+//!
+//! Will provides functions and tools to interact with Redstone's APIs
+//! [`Redstone`]: https://redstone.finance/
+
 use log::{debug, trace};
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
 
+/// Will perform a call to a specified URL and returns a vector of [ResponseApi]
+/// ToDo Make it configurable (query filter and al')
 pub async fn call(url: String, assets: Vec<String>) -> Vec<ResponseApi> {
     let req_client = Client::new();
     let response = req_client.get(url).send().await.unwrap();
@@ -11,6 +18,9 @@ pub async fn call(url: String, assets: Vec<String>) -> Vec<ResponseApi> {
     price_response
 }
 
+/// Structure representing a response from Redstone's price API
+/// ToDo Check if all fields are correctly represented
+/// [`ResponseApi`]
 #[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct ResponseApi {
     pub id: Option<String>,
@@ -59,7 +69,8 @@ mod tests {
     use super::*;
 
     #[tokio::test]
-    async fn it_works() {
+    async fn it_should_get_a_result() {
         let result = call("https://api.redstone.finance/prices?symbol=AVAX&provider=redstone-avalanche-prod-1&limit=1".parse().unwrap(), Vec::new()).await;
+        assert_ne!(result.len(), 0)
     }
 }
